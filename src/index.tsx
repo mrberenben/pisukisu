e24c0287789cb216b3a1f339b03e20b12e4602d6
eren <79411240+mrberenben@users.noreply.github.com>
2022-06-24 19:14:39 +0300

    core setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from 'App';
 import reportWebVitals from './reportWebVitals';
+import "static/styles/core.css"
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('mount') as HTMLElement
 );
 root.render(
   <React.StrictMode>
