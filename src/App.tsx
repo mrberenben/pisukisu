5f4a661060e1d93191a1c31718e64c6cc17bbcc0
eren <79411240+mrberenben@users.noreply.github.com>
2022-06-27 20:45:04 +0300

    feat: eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import styles from "static/styles/pages/index.module.css";
 
 function App() {
-  return (
-    <div className={styles.app}>
-      app
-    </div>
-  );
+  return <div className={styles.app}>app</div>;
 }
 
 export default App;
