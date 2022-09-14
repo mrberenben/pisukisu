bd8796177dad2ed8a5447bee70220cae7ac27227
eren <79411240+mrberenben@users.noreply.github.com>
2022-09-14 13:24:02 +0300

    deploy vercel

--- src/components/pages/home/Banner.tsx
+++ src/components/pages/home/Banner.tsx
@@ -3,14 +3,14 @@ import styles from "static/styles/components/pages/home/Banner.module.css";
 const Banner = () => {
   return (
     <div className={styles.banner}>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_first}`}>first anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_second}`}>second anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_third}`}>third anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_fourth}`}>fourth anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_fifth}`}>fifth anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_sixth}`}>sixth anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_seventh}`}>seventh anime</div>
-      <div className={`${styles.anime_poster} ${styles.anime_poster_eighth}`}>eighth anime</div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_first}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_second}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_third}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_fourth}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_fifth}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_sixth}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_seventh}`}></div>
+      <div className={`${styles.anime_poster} ${styles.anime_poster_eighth}`}></div>
     </div>
   );
 };
